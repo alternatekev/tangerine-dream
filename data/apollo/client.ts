8a1928fdebe903d8fccb3de8e4ee734a5acf7bab
kevinconboy <kevin@communityfunded.com>
2020-07-15 20:57:23 -0600

    trying another way

--- data/apollo/client.ts
+++ data/apollo/client.ts
@@ -8,30 +8,25 @@ let apolloClient: ApolloClient<Dispensary | {}>
 export function createApolloClient(url: string) {
   return new ApolloClient({
     ssrMode: useSSR().isServer,
-    uri: url, // Server URL (must be absolute)
+    uri: url,
     cache: new InMemoryCache(),
     headers: {
-      'Cache-Control': 'no-cache'
+      'Cache-Control': 'max-age=1'
     }
   })
 }
 
 export function initializeApollo(initialState: Dispensary | {}, wpUrl: string) {
   const _apolloClient = apolloClient ?? createApolloClient(wpUrl)
-
-  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
-  // gets hydrated here
   if (initialState) {
     _apolloClient.cache.restore(initialState)
   }
-  // For SSG and SSR always create a new Apollo Client
   if (useSSR().isServer){return _apolloClient}
-  // Create the Apollo Client once in the client
   if (!apolloClient) apolloClient = _apolloClient
 
   return _apolloClient
 }
 
-export function useApollo(initialState: Dispensary, wpUrl: string) {
+export function useApollo(initialState: Dispensary | {}, wpUrl: string) {
   return useMemo(() => initializeApollo(initialState, wpUrl), [initialState])
 }
